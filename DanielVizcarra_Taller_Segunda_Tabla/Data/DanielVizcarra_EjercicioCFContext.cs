ba7aa693f5ff584831fa74f951454d8d2f2fd633
Daniel Alberto Vizcarra Gaibor <thedavizg@gmail.com>
2024-05-08 12:15:21 -0500

    Burger y promo, App Web

---
+++ DanielVizcarra_Taller_Segunda_Tabla/Data/DanielVizcarra_EjercicioCFContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DanielVizcarra_EjercicioCF.Models;
+
+namespace DanielVizcarra_EjercicioCF.Data
+{
+    public class DanielVizcarra_EjercicioCFContext : DbContext
+    {
+        public DanielVizcarra_EjercicioCFContext (DbContextOptions<DanielVizcarra_EjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DanielVizcarra_EjercicioCF.Models.BurgerDV> BurgerDV { get; set; } = default!;
+        public DbSet<DanielVizcarra_EjercicioCF.Models.PromoDV> PromoDV { get; set; } = default!;
+    }
+}
---
+++ DanielVizcarra_Taller_Segunda_Tabla/Models/PromoDV.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DanielVizcarra_EjercicioCF.Models
+{
+    public class PromoDV
+    {
+        [Key]
+        public int PromoID { get; set; }
+        
+        public string? Descripcion { get; set; }
+        public DateTime FechaPromo { get; set; }
+
+        public int BurgerID { get; set; }
+        public BurgerDV? Burger { get; set; }
+
+    }
+}
